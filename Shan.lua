4c4a1d7d0d7e0d0c2d8ba2f99fefaf0dc15d96e0
xDestinyyx <115854394+xDestinyyx@users.noreply.github.com>
2025-01-29 11:01:14 +0800

    Update Shan.lua

--- Shan.lua
+++ Shan.lua
@@ -49,7 +49,6 @@ end)
 _G.Settings = {
 
 SelectWeapon = {"Melee"},
-FastType = {},
 AutoFarm = false,
 BringMob = true,
 FastAttack = true,
@@ -170,6 +169,36 @@ task.spawn(function()
 end)
 
 -- [ Click Functions ]
+local module = {
+    NextAttack = 0,
+    Distance = 55,
+    attackMobs = true,
+    attackPlayers = true
+}
+
+local Player = game:GetService("Players")
+
+function module:GetBladeHits()
+    local BladeHits = {}
+    for _, Enemy in game:GetService("Workspace").Enemies:GetChildren() do
+        table.insert(BladeHits, Enemy.HumanoidRootPart)
+    end
+    return BladeHits
+end
+
+function module:attack()
+    local BladeHits = self:GetBladeHits()
+    game:GetService("ReplicatedStorage").Modules.Net:WaitForChild("RE/RegisterAttack"):FireServer(0)
+    for _, Hit in BladeHits do
+        game:GetService("ReplicatedStorage").Modules.Net:WaitForChild("RE/RegisterHit"):FireServer(Hit)
+    end
+end
+
+spawn(function()
+    while wait() do
+        module:attack()
+    end
+end)
 
 function ClickCamera()
     pcall(function()
